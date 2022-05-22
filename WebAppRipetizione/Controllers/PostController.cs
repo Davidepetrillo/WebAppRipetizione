761da371eca709ec590ab330488f85cf6efd0103
davidepetrillo <davide96petrillo@gmail.com>
2022-05-22 17:43:22 +0200

    Add project files.

---
+++ WebAppRipetizione/Controllers/PostController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAppRipetizione.Models;
+using WebAppRipetizione.Models.Utils;
+
+namespace WebAppRipetizione.Controllers
+{
+    public class PostController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Post> posts = PostData.GetPosts();
+            return View("HomePage", posts);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Post postFound = null;
+            foreach (Post post in PostData.GetPosts())
+            {
+                if (post.Id == id)
+                {
+                    postFound = post;
+                    break;
+                }
+            }
+
+                if(postFound != null)
+                {
+                    return View("Details", postFound);
+                } else
+                {
+                    return NotFound($"Il post con l'Id {id} non è stato trovato");
+                }
+            
+        }
+    }
+}
---
+++ WebAppRipetizione/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿namespace WebAppRipetizione.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+
+        public Post()
+        {
+
+        }
+
+        public Post(int id, string title, string description, string image)
+        {
+            this.Id = id;
+            this.Title = title;
+            this.Description = description;
+            this.Image = image;
+        }
+    }
+
+    
+
+}
